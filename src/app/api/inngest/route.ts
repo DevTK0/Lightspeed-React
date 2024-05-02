903bfa93299fb5e915c0f7b37d6253728cb7cc95
DevTK0 <tko.developer.02@gmail.com>
2024-05-02 11:11:26 +0800

    Changed import path to be more explicit.

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/(global)/lib/inngest/client";
-import { helloWorldFn } from "../../(typef)/games/palworld/servers/[serverId]/(local)/component.service";
+
+import { helloWorldFn } from "@/app/(typef)/games/palworld/servers/[serverId]/(local)/component.service";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
